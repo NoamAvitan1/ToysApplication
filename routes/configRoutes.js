4e4a45071892ccc341de2ce2c9fdb0546ac8657a
NoamAvitan1 <129662746+NoamAvitan1@users.noreply.github.com>
2023-06-22 21:38:29 +0300

    Delete routes directory

--- routes/configRoutes.js
+++
@@ -1,9 +0,0 @@
-const indexR = require("./index");
-const usersR = require("./users");
-const toysR = require("./toys");
-
-exports.routesInit = (app) => {
-  app.use("/",indexR);
-  app.use("/users",usersR);
-  app.use("/toys",toysR);
-}
